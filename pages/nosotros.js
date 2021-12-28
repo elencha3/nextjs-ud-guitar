6ba74d377dcbd2ca4c4f9b71d052342f88e8883f
Esther Leñador <e.lenadorc@hotmail.com>
2021-12-28 12:49:59 +0100

    nosotros page

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,14 +1,48 @@
-
-import Layout from '../components/Layout'
+import Image from "next/image";
+import Layout from "../components/Layout";
+import styles from "../styles/Nosotros.module.css";
 
 const Nosotros = () => {
     return (
-        <Layout
-        page="Nosotros">
-            <h1>Desde Nosotros</h1>
-            
+        <Layout page="Nosotros">
+            <main className="container">
+                <h2 className="heading">Sobre Nosotros</h2>
+                <div className={styles.content}>
+                    <Image
+                        layout="responsive"
+                        width={600}
+                        height={450}
+                        src="/img/nosotros.jpg"
+                        alt="Imagen sobre nosotros"
+                    />
+                    <div>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Dolorem excepturi, sint iure facilis facere
+                            hic iusto consectetur, saepe, totam dignissimos ex
+                            aspernatur nisi adipisci cumque quo omnis ea dicta.
+                            Nisi! Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Dolorem excepturi, sint iure
+                            facilis facere hic iusto consectetur, saepe, totam
+                            dignissimos ex aspernatur nisi adipisci cumque quo
+                            omnis ea dicta. Nisi!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Dolorem excepturi, sint iure facilis facere
+                            hic iusto consectetur, saepe, totam dignissimos ex
+                            aspernatur nisi adipisci cumque quo omnis ea dicta.
+                            Nisi! Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Dolorem excepturi, sint iure
+                            facilis facere hic iusto consectetur, saepe, totam
+                            dignissimos ex aspernatur nisi adipisci cumque quo
+                            omnis ea dicta. Nisi!
+                        </p>
+                    </div>
+                </div>
+            </main>
         </Layout>
-    )
-}
+    );
+};
 
-export default Nosotros
+export default Nosotros;
