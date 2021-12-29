3bea30c1a741c92465a830a124586de4d11af072
Esther Leñador <e.lenadorc@hotmail.com>
2021-12-29 11:27:04 +0100

    api connection

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,26 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({entries}) => {
+
+    console.log(entries)
     return (
-        <Layout
-        page="Blog">
+        <Layout page="Blog">
             <h1>Desde Blog</h1>
-            
         </Layout>
-    )
+    );
+};
+
+//Se exporta en esta misma página. Esta función corre en el servidor
+export async function getStaticProps() {
+    const url = "http://localhost:1337/blogs";
+    const response = await fetch(url);
+    const entries = await response.json();
+
+    return {
+        props: {
+            entries: entries, //Como se llaman igual, no hace falta key & value
+        },
+    };
 }
 
-export default Blog
+export default Blog;
