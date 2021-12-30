1cf11f969ca7683a81a5f6f6d927dc8a6a5826cc
Esther Leñador <e.lenadorc@hotmail.com>
2021-12-30 12:17:32 +0100

    guitar detail styled, [url]

---
+++ pages/guitars/[url].js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Layout from "../../components/Layout";
+import styles from "../../styles/Guitar.module.css";
+
+const GuitarDetail = ({ guitar }) => {
+    const { name, description, image, price } = guitar[0];
+
+    return (
+        <Layout
+        page={`Guitarra ${name}`}>
+            <div className={styles.guitar}>
+                <Image
+                    layout="responsive"
+                    width={180}
+                    height={350}
+                    src={image.url}
+                    alt={`Imagen guitarra ${name}`}
+                />
+                <div className={styles.content}>
+                    <h3>{name}</h3>
+                    <p className={styles.description}>{description}</p>
+                    <p className={styles.price}>{price}</p>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+//El parámetro que se pasa es el nombre del archivo que has creado, en este caso [url]
+export async function getServerSideProps({ query: { url } }) {
+    const urlGuitar = `${process.env.API_URL}/guitars?url=${url}`;
+    const response = await fetch(urlGuitar);
+    const guitar = await response.json();
+    //Esta respuesta de guitar llega en un array ocn un único index.
+    return {
+        props: {
+            guitar,
+        },
+    };
+}
+
+export default GuitarDetail;
